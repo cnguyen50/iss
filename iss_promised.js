4788b3a324c2a0085e8ffab9a61a1557b583df2d
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-14 07:38:42 +0000

    added FlyOverTime function

--- iss_promised.js
+++ iss_promised.js
@@ -2,7 +2,17 @@ const request = require('request-promise-native');
 
 const fetchMyIP = function() {
     return request('https://api.ipify.org?format=json');
+}; // => {"ip":"70.79.171.222"}
+
+const fetchCoordsByIP = function(body) {
+    const ip = JSON.parse(body).ip;
+    return request(`https://ipvigilante.com/json/${ip}`);
 };
 
+const fetchISSFlyOverTimes = function(body) {
+    const { latitude, longitude } = JSON.parse(body).data;
+    return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`);
+  };
 
-module.exports = { fetchMyIP };
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
