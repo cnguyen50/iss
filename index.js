5583047381ce358db69d1c82e4357d0fc5b9eff7
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-14 00:53:27 +0000

    exported fetchCoords function

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
-const fetchMyIP = function(callback) { 
-    // use request to fetch IP address from JSON API
+const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIp } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
   }
-  
-module.exports = { fetchMyIP };
+
+  console.log('It worked! Returned IP:' , ip);
+//   console.log(typeof ip) => string
+});
+
+
+// fetchCoordsByIp("162.245.144.188", (error,data) => {
+//     if (error) {
+//         console.log("It didn't work!" , error);
+//         return;
+//       }
+//       console.log('It worked! Returned Coordinates:' , data);
+// });
+
